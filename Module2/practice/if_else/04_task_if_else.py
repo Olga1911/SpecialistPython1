45c82d6dec4ab829d0d44fd2ce76b0f2dadb7d3c
Olga1911 <91018006+Olga1911@users.noreply.github.com>
2021-09-26 12:25:29 +0300

    Update 04_task_if_else.py

--- Module2/practice/if_else/04_task_if_else.py
+++ Module2/practice/if_else/04_task_if_else.py
@@ -1,10 +1,9 @@
 Мое решение:
-
 a = int(input("a: "))
-if a//3:
+if a//3 == 0:
 	print ("Foo")
-elif a//5:
+elif a//5 == 0 :
 	print ("Bar")
-elif a//3 and a//5:
+elif a//3 == 0 and a//5 == 0:
 	print ("Foobar")
 
